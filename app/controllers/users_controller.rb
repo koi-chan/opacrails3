92de285830b61a15114e6045b8b1321415ef4c07
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-29 23:05:20 +0900

    User controller/view を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_target_user, only: [:show, :edit, :update]
+
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to(@user)
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+    @user.update(user_params)
+    @user.save
+    redirect_to(@user)
+  end
+
+  private
+
+  def user_params
+    params.
+      require(:user).
+      permit(:name)
+  end
+
+  def set_target_user
+    @user = User.find(params[:id])
   end
 end
