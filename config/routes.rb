e524756ef154d4ee96cacbd1a65379759c18d39b
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-29 19:31:32 +0900

    rails g controller Collections

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :bibliographies, only: [:index, :create, :new, :show, :edit, :update]
+  resources :collections, only: [:index, :create, :new, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
