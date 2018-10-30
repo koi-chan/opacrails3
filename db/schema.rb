365cc012341954fd272afb83ce0c6bbc768732b8
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-30 21:40:58 +0900

    2つ前の、migrate し忘れ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_103556) do
+ActiveRecord::Schema.define(version: 2018_10_30_123337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2018_10_30_103556) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bibliography_id"], name: "index_collections_on_bibliography_id"
+  end
+
+  create_table "loan_histories", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "collection_id"
+    t.date "loan_date"
+    t.date "return_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["collection_id"], name: "index_loan_histories_on_collection_id"
+    t.index ["user_id"], name: "index_loan_histories_on_user_id"
   end
 
   create_table "loans", force: :cascade do |t|
@@ -62,6 +73,8 @@ ActiveRecord::Schema.define(version: 2018_10_30_103556) do
     t.index ["user_status_id"], name: "index_users_on_user_status_id"
   end
 
+  add_foreign_key "loan_histories", "collections"
+  add_foreign_key "loan_histories", "users"
   add_foreign_key "loans", "collections"
   add_foreign_key "loans", "users"
   add_foreign_key "users", "user_statuses"
