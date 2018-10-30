66812e8f19019099065d21a2d5c4bfa20f8417d7
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-30 13:33:38 +0900

    rails g controller Loans index new show edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :collections, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: [:index, :new, :create, :show, :edit, :update]
   resources :user_statuses, only: [:index, :new, :create, :show, :edit, :update]
+  resources :loans, only: [:index, :new, :create, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
