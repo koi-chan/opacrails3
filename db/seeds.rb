d040b05ca18a9bfd4e1edc6a4ca7108d3b203155
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-30 15:07:03 +0900

    db/seed.rb を増補

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,75 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+bibliographies = Bibliography.create([
+  {
+    isbn13: 9784061473928,
+    publisher: '講談社',
+    author: 'はやみねかおる',
+    title: 'そして五人はいなくなる'
+  },
+  {
+    isbn13: 9784061484054,
+    publisher: '講談社',
+    author: 'はやみねかおる',
+    title: '亡霊は夜歩く'
+  },
+  {
+    isbn13: 9784488741013,
+    publisher: '東京創元社',
+    author: '笹本祐一',
+    title: '妖精作戦'
+  }
+])
+
+collections = Collection.create([
+  {
+    bibliography_id: 1,
+    callnum: '913.6'
+  },
+  {
+    bibliography_id: 2,
+    callnum: '913.6'
+  }
+])
+
 user_statuses = UserStatus.create([
   {
     name: '使用不可',
-    loan_days: 0
+    loan_days: 0,
+    reloan: 0
   },
   {
     name: '生徒',
-    loan_days: 7
+    loan_days: 7,
+    reloan: 1
   },
   {
     name: '教職員',
-    loan_days: 14
+    loan_days: 14,
+    reloan: 2
+  }
+])
+
+users = User.create([
+  {
+    name: '生徒1',
+    user_status_id: 1
+  },
+  {
+    name: '生徒2',
+    user_status_id: 1
+  },
+  {
+    name: '先生1',
+    user_status_id: 2
+  }
+])
+
+loans = Loan.create([
+  {
+    user_id: 1,
+    collection_id: 1,
+    loan_date: Date.today,
   }
 ])
