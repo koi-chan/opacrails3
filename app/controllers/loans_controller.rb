d0d72045c4187b8acb81181800861c217198262a
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-30 14:26:17 +0900

    最低限の貸出機能の実装

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,13 +1,50 @@
 class LoansController < ApplicationController
+  before_action :set_target_loan, only: [:show, :edit, :update]
+
   def index
+    @loans = Loan.all
   end
 
   def new
+    @loan = Loan.new
+    @users = User.all
+    @collections = Collection.all
+  end
+
+  def create
+    @loan = Loan.new(loan_params)
+    @loan.loan_date = Date.today
+    @loan.return_date = @loan.loan_date + @loan.user.user_status.loan_days
+    @loan.reloan_count = 0
+    @loan.save
+    redirect_to(@loan)
   end
 
   def show
   end
 
   def edit
+    @users = User.all
+    @collections = Collection.all
+  end
+
+  def update
+    @loan.update(loan_params)
+    @loan.return_date = @loan.loan_date + @loan.user.user_status.loan_days
+    @loan.reloan_count += 1
+    @loan.save
+    redirect_to(@loan)
+  end
+
+  private
+
+  def loan_params
+    params.
+      require(:loan).
+      permit(:user_id, :collection_id, :loan_date, :return_date, :reloan_count)
+  end
+
+  def set_target_loan
+    @loan = Loan.find(params[:id])
   end
 end
