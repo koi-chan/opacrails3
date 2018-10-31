908b3ab1e92d768b4c05f931e20e25fca9394337
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-31 23:49:16 +0900

    Publisher の isbn_code のバリデーションを追加

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,9 @@
 class Publisher < ApplicationRecord
+
+  validates(:place, presence: true)
+  validates(:name, presence: true)
+  validate(:isbn_code, :validate_isbn_code)
+
   def publication
     "#{self.place} : #{self.name}"
   end
@@ -6,6 +11,12 @@ class Publisher < ApplicationRecord
   def self.publications
     self.all.map do |t|
       t.publication
+    end
+  end
+
+  def validate_isbn_code
+    unless(self.isbn_code =~ /\A(?:0\d{1}|[1-9]\d{1,6})\z/)
+      errors.add(isbn_code: 'ISBN 出版者コードが正しくありません')
     end
   end
 end
