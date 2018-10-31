75740c84646c1b0519404575313d13af37e486a5
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-31 11:15:15 +0900

    貸出状態を調べるメソッドを Collection モデルに追加

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,13 @@
 class Collection < ApplicationRecord
   belongs_to :bibliography
+
+  # 貸し出し中なら返却期限を返す
+  # @return [Date, nil]
+  def loaned?
+    if loan = Loan.find_by(collection_id: self.id)
+      loan.return_date
+    else
+      nil
+    end
+  end
 end
