0ae7bf771369c92cd41b7f7b5cd07bf068e8e8b4
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-31 13:43:02 +0900

    トップページを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :bibliographies, only: [:index, :new, :create, :show, :edit, :update]
-  resources :collections, only: [:index, :new, :create, :show, :edit, :update]
-  resources :users, only: [:index, :new, :create, :show, :edit, :update]
-  resources :user_statuses, only: [:index, :new, :create, :show, :edit, :update]
-  resources :loans, only: [:index, :new, :create, :show, :edit, :updatei, :destroy]
-  resources :loan_histories, only: [:index, :show]
+  root 'welcome#index'
+
+  resources :bibliographies, only: %i(index new create show edit update)
+  resources :collections, only: %i(index new create show edit update)
+  resources :users, only: %i(index new create show edit update)
+  resources :user_statuses, only: %i(index new create show edit update)
+  resources :loans, only: %i(index new create show edit update destroy)
+  resources :loan_histories, only: %i(index show)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
