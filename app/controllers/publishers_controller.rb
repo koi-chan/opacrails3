4c80c9e2daf7d60204de7d3e96dacb83c506a93b
koi-chan <koi-chan@mail.kazagakure.net>
2018-10-31 17:30:15 +0900

    rails g controller publishers index new show edit

---
+++ app/controllers/publishers_controller.rb
@@ -0,0 +1,13 @@
+class PublishersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :bibliographies, only: %i(index new create show edit update)
+  resources :publishers, only: %i(index new create show edit update)
   resources :collections, only: %i(index new create show edit update)
   resources :users, only: %i(index new create show edit update)
   resources :user_statuses, only: %i(index new create show edit update)
